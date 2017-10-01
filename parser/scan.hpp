256df0aecaf847b61d3e999ffaf32d74386542b2
chupotle <tcchu97@gmail.com>
2017-10-01 15:42:31 -0400

    fixed declaration for enum token

--- parser/scan.hpp
+++ parser/scan.hpp
@@ -2,8 +2,9 @@
     Michael L. Scott, 2008-2017.
 */
 
-typedef enum {t_read, t_write, t_id, t_literal, t_gets, t_if, t_fi, t_do, t_od, t_check, t_ro, t_eq, t_neq, t_lthan, t_gthan, t_leq, t_geq,
-                t_add, t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof} token;
+enum token{t_read, t_write, t_id, t_literal, t_gets, t_if, t_fi, t_do,
+  t_od, t_check, t_ro, t_eq, t_neq, t_lthan, t_gthan, t_leq, t_geq,
+  t_add, t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof} token;
 
 extern char token_image[100];
 
